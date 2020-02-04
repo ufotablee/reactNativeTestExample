3d09d3129f598917bc08eb431e1c3506a5f0df14
mksninja <kitavaurod@gmail.com>
2020-02-04 17:37:55 +0200

    33

--- App.js
+++ App.js
@@ -1,18 +1,24 @@
 import React from 'react';
 import styled from 'styled-components/native'
+import { StyleSheet, Text, SafeAreaView, ScrollView } from 'react-native';
 import { Gallery } from './containers' 
 import { Provider } from 'react-redux'
 import store from "./redux/store"
-
+import Constants from 'expo-constants';
 const App = () => {
   return (
     <Provider store={store}>
+      <SafeAreaView >
+      <ScrollView  >
       <Container>
       <Gallery />
       </Container>
+      </ScrollView>
+    </SafeAreaView>
     </Provider>
   );
 }
+
 const Container = styled.View`
  flex:1;
  width: 80%;
--- components/GalleryItem/index.js
+++ components/GalleryItem/index.js
@@ -9,11 +9,11 @@ const GalleryItem = (item) => {
 const [visiable, setIsVisiable ] = useState(false)
 return (
     <GroupItem onPress={() => setIsVisiable(!visiable)}>
+        <GroupImg source={{ uri : `${item.links.download}`}} />
         {visiable && 
         <FullPage url={item.links.download} visiable={visiable} setIsVisiable={setIsVisiable}/>
         }
-        <GroupImg source={{ uri : `${item.links.download}`}}/>
-        <Text>{item.user.first_name} {item.user.last_name}</Text>
+        <GroupText>{item.user.first_name} {item.user.last_name}</GroupText>
     </GroupItem>
 )
 }
@@ -21,7 +21,9 @@ const GroupImg = styled.Image`
 width: 150px;
 height: 150px;
 `;
-
+const GroupText = styled.Text`
+color: #00a0fc
+font-size: 15px`;
 const GroupItem = styled.TouchableOpacity`
 align-items: center;
 padding: 20px 0;
